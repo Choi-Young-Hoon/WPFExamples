170ce671c4e7cea79efe2198c8ac0076f2416fa9
Choi-Young-Hoon <hun5077@gmail.com>
2023-09-01 18:15:23 +0900

    MVVM pattern

---
+++ WPFStudy/MVVMPattern/Model/MainModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMPattern.Model
+{
+    // 모델을 정의하기 위해 INotifyPropertyChanged 를 상속받는다.
+    internal class MainModel : INotifyPropertyChanged
+    {
+        private int _inputNumber;
+        public int InputNumber
+        {
+            get => this._inputNumber;
+            set
+            {
+                this._inputNumber = value;
+                this.OutputNumber = value * 2;
+                OnPropertyChanged("InputNumber");// UI에 InputNumber를 갱신한다.
+            }
+        }
+
+        private int _outputNumber;
+        public int OutputNumber
+        {
+            get => this._outputNumber;
+            set
+            {
+                this._outputNumber = value;
+                OnPropertyChanged("OutputNumber"); // UI에 OutputNumber를 갱신한다.
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WPFStudy/MVVMPattern/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MVVMPattern.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMPattern.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private MainModel _model = null;
+
+        public MainViewModel()
+        {
+            this._model = new MainModel();
+        }
+
+        public MainModel Model
+        {
+            get => this._model;
+            set
+            {
+                this._model = value;
+                OnPropertyChanged("Model");
+            }
+            
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
